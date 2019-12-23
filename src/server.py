530a90086dd05d007158bf116b3a0eabbaf51b81
Andrew Ashraf <andrew.achraf44@gmail.com>
2019-12-23 20:49:44 +0200

    bug fixed minor minor minor minor

--- src/server.py
+++ src/server.py
@@ -17,8 +17,8 @@ app=Flask(__name__);
 @app.route('/messaging', methods =['GET','POST'])
 def chatService():
     clientMessage = request.get_json();
-    print clientMessage
-    print clientMessage['Message'];
+    print (clientMessage)
+    print (clientMessage['Message'])
     botResponse = chat(clientMessage['Message']);
     return botResponse;
 
